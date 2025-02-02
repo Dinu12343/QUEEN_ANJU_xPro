edcd2749352141d0528046f4e92eec5a146ad6d4
Dinu12343 <162849493+Dinu12343@users.noreply.github.com>
2025-02-02 16:07:59 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,6 +40,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~KMc1xD6S#-duISNCnAJFrQzHjk0yhcgYbcTzyve6eM3e8dUzzL7w",
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~yMsi0I7B#roNkXnOwVDQP39oEWJGZoPG-kKdynOOtHIaTPsmria4",
 PORT: process.env.PORT || "8000"
 };
